69beba03bb0c28632a9407599905269946963cd4
Siphuuu <Sifanisraelt@gmail.com>
2022-12-11 11:59:49 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - single digit number of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
