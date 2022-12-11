4ad691daf535a1803b218f34fdd02153461e3c85
Siphuuu <Sifanisraelt@gmail.com>
2022-12-11 12:57:30 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphabets in reverse order
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
