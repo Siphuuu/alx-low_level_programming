e232fb2e260375b8f7fb8049b25e634255f8e32f
Siphuuu <Sifanisraelt@gmail.com>
2022-12-12 05:50:15 -0800

    The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 		{
 			for (once = '0'; tens <= '9'; once++)
 			{
-				if (!((once == tens == hundreds) || hundreds > tens > once))
+				if (!(once == tens || tens == hundreds || hundreds > tens || tens > once))
 				{
 					putchar(hundreds);
 					putchar(tens);
