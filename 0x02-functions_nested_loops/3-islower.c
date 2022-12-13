dfb6c2b8ad471e4e222f9c2b6a91571e4444b697
Siphuuu <Sifanisraelt@gmail.com>
2022-12-13 12:34:18 -0800

    lower alpha case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * islower - cheacks if alpha is lower case
+ * _islower - cheacks if alpha is lower case
  * @c: the character
  * Return: 1 if its lowercase, 0 if else
  */
-int _islower(int c);
-{
-	char c;
 
+int _islower(int c)
+{
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
