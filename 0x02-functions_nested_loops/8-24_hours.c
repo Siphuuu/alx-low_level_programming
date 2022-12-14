757573d97affc2834af9c4510cd7536191094885
Siphuuu <Sifanisraelt@gmail.com>
2022-12-13 16:15:26 -0800

    i amfederal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,9 @@ void jack_bauer(void)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
-			_putcharu(':');
-			_putchar(m / 10) +
-				'0');
-			_putchar(m % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
