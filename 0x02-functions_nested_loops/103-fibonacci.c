47bfcf4bc8936363a8281c07a00a22f298bedfbe
Siphuuu <Sifanisraelt@gmail.com>
2022-12-14 18:31:01 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci number sum that dont exceed 4000000
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, l = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+		{
+			l += k;
+			j = k - j;
+		}
+		++i;
+	}
+	printf("%ld\n", l);
+	return (0);
+}
