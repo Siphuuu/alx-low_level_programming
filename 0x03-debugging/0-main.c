620d1328e1572f86a08954446396892fdef1301e
Siphuuu <Sifanisraelt@gmail.com>
2022-12-15 03:50:30 -0800

    multi main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,40 @@
-#include "main.h"
-
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+#include <stdlib.h>                                                                                                                                                       
+#include <time.h>                                                                                                                                                         
+#include <stdio.h>                                                                                                                                                        
+                                                                                                                                                                          
+/**                                                                                                                                                                       
+ * positive_or_negative - cheacks whether a number is positive or negative                                                                                                
+ * @i: integer                                                                                                                                                            
+ * Return: Always 0                                                                                                                                                       
+ */                                                                                                                                                                       
+                                                                                                                                                                          
+void positive_or_negative(int i)                                                                                                                                          
+                                                                                                                                                                          
+{                                                                                                                                                                         
+        int n;                                                                                                                                                            
+                                                                                                                                                                          
+        sran(time(0));                                                                                                                                                    
+        n = i;                                                                                                                                                            
+                                                                                                                                                                          
+        if (n > 0)                                                                                                                                                        
+                printf("%i is positive\n", n);                                                                                                                            
+        else if (n < 0)                                                                                                                                                   
+                printf("%i is negative\n", n);                                                                                                                            
+        else                                                                                                                                                              
+                printf("%i is zero\n", n);                                                                                                                                
+        return (0);                                                                                                                                                       
+}                                                                                                                                                                         
+                                                                                                                                                                          
+/**                                                                                                                                                                       
+ * main - debugging example                                                                                                                                               
+ * Return: Always 0                                                                                                                                                       
+ */                                                                                                                                                                       
+                                                                                                                                                                          
+int main(void)                                                                                                                                                            
+{                                                                                                                                                                         
+        int i;                                                                                                                                                            
+                                                                                                                                                                          
+        i = 0;                                                                                                                                                            
+        positive_or_negative(i);                                                                                                                                          
+        return (0);                                                                                                                                                       
 }
