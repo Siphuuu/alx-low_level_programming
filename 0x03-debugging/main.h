543d92bee68422c41644c8e57f9418c9b7781658
Siphuuu <Sifanisraelt@gmail.com>
2022-12-16 04:55:49 -0800

    o task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 
 int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
 
 #endif /* MAIN_H */
