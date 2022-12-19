2d87658557f011039b7339599c4063c25c4ff215
Siphuuu <Sifanisraelt@gmail.com>
2022-12-19 14:55:10 -0800

    Half the lies they tell about me a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ * @str: string
+ * Return: empty
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
