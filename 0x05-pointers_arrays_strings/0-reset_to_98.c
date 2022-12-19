f891ce93fe06cb845f76ecd87641f321c1ade64f
Siphuuu <Sifanisraelt@gmail.com>
2022-12-19 12:19:03 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take ponter to parameter
+ * the value to 98
+ * @n: poniter
+ * Return: empty
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
