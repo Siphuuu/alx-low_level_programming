750f30684a69130a31dd1d7fb317dea7e1a914b6
Siphuuu <Sifanisraelt@gmail.com>
2023-01-12 07:00:13 -0800

    concacinate string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - string length
+ * @str: string
+ * Return: lenght of the string
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * string_nconcat - concasinate two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: integer
+ * Return: new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int i, j, num, len;
+
+	num = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) * len);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
+}
